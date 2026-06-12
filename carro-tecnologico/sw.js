bb1852790e555cc12d1c1204c73766d053e14f35
apdocentepba-hub <apdocentepba@gmail.com>
2026-06-12 11:34:54 -0300

    Agregar service worker del carro tecnológico

---
+++ carro-tecnologico/sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'carro-tecnologico-v1';
+const APP_SHELL = [
+  './',
+  './index.html',
+  './tema-lindo.css?v=1',
+  './manifest.webmanifest?v=1'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(APP_SHELL))
+      .catch(() => null)
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  const url = new URL(event.request.url);
+  if (url.origin !== location.origin) return;
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy)).catch(() => null);
+        return response;
+      })
+      .catch(() => caches.match(event.request).then(cached => cached || caches.match('./index.html')))
+  );
+});
