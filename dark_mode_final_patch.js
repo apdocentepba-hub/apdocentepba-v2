059999d3abc42c52d9fc159b59356f2ddf2dd96c
apdocentepba-hub <apdocentepba@gmail.com>
2026-06-22 11:17:38 -0300

    Disable dark mode patch loader

--- dark_mode_final_patch.js
+++ dark_mode_final_patch.js
@@ -2,12 +2,4 @@
 'use strict';
 if(window.__apdDarkModeFinalPatchLoaded) return;
 window.__apdDarkModeFinalPatchLoaded = true;
-function load(src){
-  var s=document.createElement('script');
-  s.src=src;
-  s.defer=true;
-  document.head.appendChild(s);
-}
-load('./secretaria_publicidad_patch.js?v=1');
-load('./side_banners_patch.js?v=1');
 })();
