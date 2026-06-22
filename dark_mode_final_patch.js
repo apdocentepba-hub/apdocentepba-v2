09b33b276c0f803b0d56d5a3560b4b259c9f404d
apdocentepba-hub <apdocentepba@gmail.com>
2026-06-22 10:28:14 -0300

    Load side banners patch safely

--- dark_mode_final_patch.js
+++ dark_mode_final_patch.js
@@ -2,5 +2,12 @@
 'use strict';
 if(window.__apdDarkModeFinalPatchLoaded) return;
 window.__apdDarkModeFinalPatchLoaded = true;
-import('./secretaria_publicidad_patch.js?v=1').catch(function(){});
+function load(src){
+  var s=document.createElement('script');
+  s.src=src;
+  s.defer=true;
+  document.head.appendChild(s);
+}
+load('./secretaria_publicidad_patch.js?v=1');
+load('./side_banners_patch.js?v=1');
 })();
